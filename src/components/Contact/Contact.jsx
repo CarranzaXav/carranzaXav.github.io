8549063238de10ca2c4443476f0b33edcaf756d1
CarranzaXav <carranzax7@gmail.com>
2024-02-05 15:51:04 -0500

    2nd commit on 02-5-2024

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './Contact.module.css';
+
+import { getImageUrl } from '../../utils';
+
+export const Contact = () => {
+    return (
+        <footer id='contact' className={styles.container}>
+            <div className={styles.text}>
+                <h2 className={styles.title}>Contact</h2>
+                < p > Feel free to reach out!</p >
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                    <a href="mailto:carranzax7@gmail.com"> carranzax7@gmail.com</a></li>
+                <li className={styles.link}>
+                    <img src={getImageUrl('contact/githubIcon.png')} alt="Github Icon" />
+                    <a href="">github.com</a></li>
+                <li className={styles.link}>
+                    <img src={getImageUrl('contact/linkedinIcon.png')} alt="LinkedIn icon" />
+                    <a href="">LinkedIn</a></li>
+            </ul>
+        </footer >
+    )
+}

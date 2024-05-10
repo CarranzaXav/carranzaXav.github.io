b39cc221b64ab9f932064615588e774e1b42f56a
CarranzaXav <carranzax7@gmail.com>
2024-05-10 16:23:37 -0400

    May Update 1.6

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,14 +12,22 @@ export const Contact = () => {
             </div>
             <ul className={styles.links}>
                 <li className={styles.link}>
-                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-                    <a href="mailto:carranzax7@gmail.com"> carranzax7@gmail.com</a></li>
+                    <a href="mailto:carranzax7@gmail.com"> 
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" /> 
+                     <div>carranzax7@gmail.com</div>
+                     </a>
+                    </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl('contact/githubIcon.png')} alt="Github Icon" />
-                    <a href="https://github.com/CarranzaXav">https://github.com/CarranzaXav</a></li>
+            
+                    <a href="https://github.com/CarranzaXav">
+                        <img src={getImageUrl('contact/githubIcon.png')} alt="Github Icon" /> 
+                        <div>https://github.com/CarranzaXav</div>
+                        </a>
+                     </li>
                 <li className={styles.link}>
-                    <img src={getImageUrl('contact/linkedinIcon.png')} alt="LinkedIn icon" />
-                    <a href="https://www.linkedin.com/in/xavier-carranza-458690218">LinkedIn</a></li>
+                    
+                    <a href="https://www.linkedin.com/in/xavier-carranza-458690218">
+                    <img src={getImageUrl('contact/linkedinIcon.png')} alt="LinkedIn icon" /> <div>LinkedIn</div></a></li>
             </ul>
         </footer >
     )

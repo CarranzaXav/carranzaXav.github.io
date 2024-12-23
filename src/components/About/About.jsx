3e6ba3534000158f9a341325b856adbb13397549
CarranzaXav <carranzax7@gmail.com>
2024-12-22 21:36:10 -0500

    Portfolio Update 12/22/2024

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,24 +6,25 @@ import { getImageUrl } from '../../utils';
 
 export const About = () => {
     return <section className={styles.container} id="about">
-        <h2 className={styles.title}>About</h2>                                                                                                                                                                                                                                                 
+        <h2 className={styles.title}>About</h2>
+
         <div className={styles.content} id="position">
-                    <img
-                        src={getImageUrl("about/cursorIcon.png")}
-                        className={styles.aboutCursorIcon}
-                        alt="Cursor icon"
-                        id="a"
-                        loading="eager"
-                    />
-                    <div className={styles.aboutItem} id="b">
-                        <p>
-Dedicated to the principle of continual learning, I navigate the realm of full stack development with fervor, particularly drawn to JavaScript, React, and the vibrant domain of web development. It's the fusion of creativity, problem-solving, and the ever-evolving tech landscape that fuels my excitement and keeps me hooked on the journey. </p>
-                    </div>
-                 <img
+            <img
+                src={getImageUrl("about/cursorIcon.png")}
+                className={styles.aboutCursorIcon}
+                alt="Cursor icon"
+                id="a"
+                loading="eager"
+            />
+            <div className={styles.aboutItem} id="b">
+                <p>
+                    Dedicated to the principle of continual learning, I navigate the realm of full stack development with fervor, particularly drawn to JavaScript, React, and the vibrant domain of web development. It's the fusion of creativity, problem-solving, and the ever-evolving tech landscape that fuels my excitement and keeps me hooked on the journey. </p>
+            </div>
+            <img
                 src={getImageUrl("about/aboutImage.jpg")}
                 alt="me sitting with a laptop"
                 id="c"
-                className={styles.aboutImage}/>
+                className={styles.aboutImage} />
         </div>
     </section>;
 };
--- src/components/Navbar/NavbarContainer.jsx
+++ src/components/Navbar/NavbarContainer.jsx
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
-// import Bootstrap from 'bootstrap'
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import styles from "./NavbarContainer.module.css";
 
@@ -12,19 +10,19 @@ function NavbarContainer() {
   return (
     <Navbar expand="lg">
       <Container className='pt-3 me-1 fixed-top d-flex flex-row-reverse'>
-          <div className={`${styles.navBlock}`}>
-            <Navbar.Brand href="#home" className='d-flex pe-3 mx-auto p-4 flex-grow-1'></Navbar.Brand>
-            <div >
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mx-auto pt-2">
-                    <Nav.Link href="#home" className='text-white mx-auto'>Home</Nav.Link>
-                    <Nav.Link href="#about" className='text-white mx-auto'>About</Nav.Link>            
-                    <Nav.Link href="#projects" className='text-white mx-auto'>Projects</Nav.Link>
-                    <Nav.Link href="#contact" className='text-white mx-auto'>Contact</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-              </div>
+        <div className={`${styles.navBlock}`}>
+          <Navbar.Brand href="#home" className='d-flex pe-3 mx-auto p-4 flex-grow-1'></Navbar.Brand>
+          <div >
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mx-auto pt-2">
+                <Nav.Link href="#home" className='text-white mx-auto'>Home</Nav.Link>
+                <Nav.Link href="#about" className='text-white mx-auto'>About</Nav.Link>
+                <Nav.Link href="#projects" className='text-white mx-auto'>Projects</Nav.Link>
+                <Nav.Link href="#contact" className='text-white mx-auto'>Contact</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </div>
       </Container>
     </Navbar>
